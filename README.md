d415bfd1adc17e8af88229d233178fa395b286ac
TheCodedHuman <mokshmahawar05@gmail.com>
2025-07-14 14:52:58 +0530

    add multer and cloudinary files for the upload

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 In this repository I learned full stack development (focused on backend) by chai aur javascript backend playlist of Hitesh Sir | Chai Aur Code
 
 - [Model Link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
+    
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,7 +1,7 @@
 class ApiError extends Error {
     constructor(
         statusCode,
-        message = "Something went wrong",
+        message = "Something went wrong",               // no context message (not optimal)
         errors = [],
         stack = ""
     ) {
